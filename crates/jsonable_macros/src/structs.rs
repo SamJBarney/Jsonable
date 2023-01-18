21a7974fc8c6810d8d4f322d0145b47c706a9199
Samuel Barney <samjbarney@gmail.com>
2023-01-17 20:40:49 -0700

    Fixed Unit Struct JSON representation

--- crates/jsonable_macros/src/structs.rs
+++ crates/jsonable_macros/src/structs.rs
@@ -139,23 +139,23 @@ pub fn implement_unit(identifier: &Ident) -> TokenStream {
         impl jsonable::Jsonable for #identifier {
             fn from_json_unchecked(json: serde_json::Value) -> Self {
                 let inner_json = json
-                    .as_object()
-                    .unwrap_or_else(|| panic!("Tried converting non-object json to {}", #ident_str));;
+                    .as_null()
+                    .unwrap_or_else(|| panic!("Tried converting non-null json to {}", #ident_str));;
                 Self
             }
 
             fn to_json(&self) -> serde_json::Value {
-                serde_json::Value::Object(serde_json::Map::new())
+                serde_json::Value::Null
             }
 
             fn validate_json(json: &serde_json::Value) -> jsonable::Result<()> {
                 match json {
-                    serde_json::Value::Object(_) => Ok(()),
-                    serde_json::Value::Array(_) => Err(jsonable::JsonableError::IncompatibleJsonType { got: "array", expected: "object" }),
-                    serde_json::Value::Bool(_) => Err(jsonable::JsonableError::IncompatibleJsonType { got: "bool", expected: "object" }),
-                    serde_json::Value::Null => Err(jsonable::JsonableError::IncompatibleJsonType { got: "null", expected: "object" }),
-                    serde_json::Value::Number(_) => Err(jsonable::JsonableError::IncompatibleJsonType { got: "number", expected: "object" }),
-                    serde_json::Value::String(_) => Err(jsonable::JsonableError::IncompatibleJsonType { got: "string", expected: "object" })
+                    serde_json::Value::Null => Ok(()),
+                    serde_json::Value::Array(_) => Err(jsonable::JsonableError::IncompatibleJsonType { got: "array", expected: "null" }),
+                    serde_json::Value::Bool(_) => Err(jsonable::JsonableError::IncompatibleJsonType { got: "bool", expected: "null" }),
+                    serde_json::Value::Object(_) => Err(jsonable::JsonableError::IncompatibleJsonType { got: "object", expected: "null" }),
+                    serde_json::Value::Number(_) => Err(jsonable::JsonableError::IncompatibleJsonType { got: "number", expected: "null" }),
+                    serde_json::Value::String(_) => Err(jsonable::JsonableError::IncompatibleJsonType { got: "string", expected: "null" })
                 }
             }
         }
